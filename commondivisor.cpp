fe5c16fa093648b6dfa6693a88f1ea53968ada2b
AditiNanda <aryannanda97@gmail.com>
2022-05-19 23:10:05 +0530

    Common divisor program

---
+++ commondivisor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=25,b=15;
+    // int temp;
+    // if(a>b){
+    //     temp=a;
+    // }
+    // else{
+    //     temp=b;
+    // }
+    // for(int i=temp; i>1; i--){
+    //     if(a%i==0 && b%i==0){
+    //         cout<<i<<" ";
+    //         break;
+    //     }  
+    // }
+    int gcd;
+    for(int i=1; i<=a && i<=b; i++){
+        if(a%i==0 && b%i==0){
+            gcd=i;
+        }
+    }
+    cout<<gcd;
+    return 0;
+}
